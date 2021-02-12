0cd871b3d09e198a141e03f1d4a49dfe9cab23d0
ROBYN PERRIN <robynperrin76@gmail.com>
2021-02-12 15:35:55 +0100

    cloudinary added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -39,6 +39,6 @@ class CocktailsController < ApplicationController
    private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :address, :phone_number, :category)
+    params.require(:cocktail).permit(:name, :photos)
   end
 end
